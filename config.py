d226fd4a135980d955592fd7c43267bde9449f9b
Saurav8651 <161611396+Saurav8651@users.noreply.github.com>
2024-03-12 07:57:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
+API_ID = int(environ.get("API_ID", "26387767"))
+API_HASH = environ.get("API_HASH", "d02f58640448a5ec63b63a679c1643d1")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6993860625:AAEgyN-JHD-y3lHcXJnPzOTav2yjX8c1-Yk")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002132856354"))
+ADMINS = int(environ.get("ADMINS", "6215438910"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://hackerboymzf:3-Sb9-.CL5jbS4j@cluster0.0pvztkd.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
